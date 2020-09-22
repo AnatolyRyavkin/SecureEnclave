3790598d7d86b5ba180acf1b24de7639fcc90521
AnatolyRyavkin <toryavkin@gmail.com>
2020-09-22 12:42:12 +0300

    Initial Commit

---
+++ Secure Enclave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Secure Enclave
+//
+//  Created by Anatoly Ryavkin on 22.09.2020.
+//  Copyright © 2020 AnatolyRyavkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
